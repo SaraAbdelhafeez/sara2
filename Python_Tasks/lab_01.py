dbbfdc12ee54f45b0cdb4d8fe7b32acbb4f3c268
SaraAbdelhafeez <116598417+SaraAbdelhafeez@users.noreply.github.com>
2023-01-07 04:33:46 +0200

    Update lab_01.py

--- Python_Tasks/lab_01.py
+++ Python_Tasks/lab_01.py
@@ -1,13 +1,16 @@
 # Sara Mahmoud Abdelhafeez
 # abdelhafeezsara73@gmail.com
 
-def mutate_string(string, index, character):
-
-    string = string[:index] + character + string[index + 1:]
-    return string
-
+# Receiving the variables from the user
 string = input("String: ")
 character = input("Character: ")
 position = input("Position: ")
 
-print(mutate_string(string, int(position), character))
+
+def mutate_string(string, index, character):
+    string = string[:index] + character + string[index + 1:]    # Combine the string from the begining to the specified index -1 
+                                                                # then add the character in the index
+                                                                # then combine the rest of string
+    return string
+
+print(mutate_string(string, int(position), character))          # Receiving the new string and print it 
