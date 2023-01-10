26524d49efa5ee92c712b3f6cdf409c842802f04
SaraAbdelhafeez <116598417+SaraAbdelhafeez@users.noreply.github.com>
2023-01-11 00:26:56 +0200

    Add files via upload

---
+++ Python_Tasks/lab_03.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        mapping = {
+            '(':')',
+            '[':']',
+            '{':'}'            
+        }
+        
+        for char in s:
+            if char in mapping.keys():
+                stack.append(mapping[char])
+            elif not stack or stack[-1] != char:
+                return False
+            else:
+                stack.pop()
+        
+        return not stack
+
+obj = Solution()
+
+print(obj.isValid('({[]})'))
